94ba2373382f64b5696afcfb898a054449860d90
shaheerkhan00 <shaheer.khan.sk47@gmail.com>
2025-12-14 03:36:24 -0500

    planner node added and tested

--- agent/graph.py
+++ agent/graph.py
@@ -2,19 +2,24 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from states import Plan
 from prompts import genSysPrompt
-
+from langgraph.constants import END
+from langgraph.graph import StateGraph
 
 load_dotenv()
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
-user_prompt= "create a simple web app for calculator"
+
 #planning node of lang graph
+def planner_agent(state: dict) -> dict:
+    user_prompt = state["user_prompt"]
+    plan_resp = llm.with_structured_output(Plan).invoke(genSysPrompt(user_prompt))
+    return {'plan':plan_resp}
 
-system_planning_prompt = genSysPrompt(user_prompt)
-plan_resp = llm.with_structured_output(Plan).invoke(system_planning_prompt)
+graph = StateGraph(dict)
+graph.add_node("planner",planner_agent)
+graph.set_entry_point("planner")
 
-#resp = llm.with_structured_output(Schema).invoke(
- #   "Extract price and EPS from this: Toyota's new model is priced at $53,000. The company reported EPS of $8.50 for Q4."
-#)
-
-print(plan_resp)
+agent = graph.compile()
+user_prompt= "create a simple web app for calculator"
+result = agent.invoke({"user_prompt":user_prompt})
+print(result)
